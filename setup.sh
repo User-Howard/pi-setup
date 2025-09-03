cc71a3be169c2d8a1731e2fd5292eb05b8735f00
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2025-09-03 19:50:25 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,3 +18,16 @@ sudo systemctl enable --now tailscaled
 
 # Install Podman and Podman Compose
 sudo apt install -y podman podman-compose
+
+PUB_KEYS=(
+"SHA256:KFjZao0NTyJokBztXwL6COA0FBlwxssZDgFBKhR4Vmw howard@pi"
+)
+
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+
+for key in "${PUB_KEYS[@]}"; do
+    grep -qxF "$key" ~/.ssh/authorized_keys || echo "$key" >> ~/.ssh/authorized_keys
+done
+
+chmod 600 ~/.ssh/authorized_keys
