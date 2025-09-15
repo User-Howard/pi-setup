45c325cf853bff8ec94fb8bad5b29dc4bf490feb
Hao-wei Wu <60650989+User-Howard@users.noreply.github.com>
2025-09-15 18:36:32 +0800

    Add Docker installation to setup script
    Added installation steps for Docker to setup.sh.

--- setup.sh
+++ setup.sh
@@ -19,6 +19,22 @@ sudo systemctl enable --now tailscaled
 # Install Podman and Podman Compose
 sudo apt install -y podman podman-compose
 
+# Install Docker
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
 # install lazyvim
 cd /tmp
 wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-arm64.tar.gz
